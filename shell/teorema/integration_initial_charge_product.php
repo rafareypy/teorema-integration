58cb08936479f69b4e4121bad3371acb46ac58e8
rafael <rreynoud@gmail.com>
2015-06-01 17:54:32 -0300

    Adicionando script php para carga inicial dos produtos

---
+++ shell/teorema/integration_initial_charge_product.php
@@ -0,0 +1,27 @@
+<?php
+// declarando o magento
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+chdir(dirname(__FILE__));
+require '../../app/Mage.php' ;
+umask(0);
+Mage::app()->getStore();
+
+
+//integration_initial_charge_product
+
+
+	echo "\n Este script tem a função de criar todos os produtos da tabela teorema_integration_initial   \n";
+	echo "\n para o Magento   \n";
+
+	echo "\n Vamos iniciar a carga de produtos..  \n";
+
+
+	$service = Mage::getModel('teorema_integration/service_product');
+
+	$service->initialCharge();
+
+	echo "\nTerminamos a execução\n";
+
+
+?>
